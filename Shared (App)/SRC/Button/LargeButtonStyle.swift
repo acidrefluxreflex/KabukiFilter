1699493f11714df6fdaa6463dd505bee936756d6
kabuki <tanuki5sanuki3@icloud.com>
2023-10-24 12:21:11 +0900

    ho

---
+++ Shared (App)/SRC/Button/LargeButtonStyle.swift
@@ -0,0 +1,47 @@
+//
+//  LargeButtonStyle.swift
+//  KabukiFilter
+//
+//  Created by Kabuki Iwashita on 2023/09/29.
+//
+
+import SwiftUI
+
+struct LargeButtonStyle: View {
+
+    @State private var color: Color
+    @State private var text: String
+    @State private var systemName: String
+
+    private let size: CGFloat = 100
+
+    init(
+        color: Color = .accentColor,
+        text: String = "Hello",
+        systemName: String = "key.fill"
+    ) {
+        self.color = color
+        self.text = text
+        self.systemName = systemName
+    }
+
+    var body: some View {
+        VStack {
+            Text(text)
+                .font(.callout)
+            Image(systemName: systemName)
+                .font(.largeTitle)
+                .frame(width: size - 40, height: size - 40)
+        }.frame(width: size, height: size * 1.12)
+            .foregroundColor(.white)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+            )
+    }
+}
+
+struct LargeButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        LargeButtonStyle()
+    }
+}
